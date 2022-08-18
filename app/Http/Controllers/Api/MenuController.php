5031285b0e1735ea3e3168599db560c8ba7f77e1
RafaAhdiyaka <rafaahdiyaka@gmail.com>
2022-08-18 20:20:37 +0700

    Update dan Delete Data API

--- app/Http/Controllers/Api/MenuController.php
+++ app/Http/Controllers/Api/MenuController.php
@@ -6,6 +6,7 @@ use App\Models\Menu;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MenuResource;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class MenuController extends Controller
@@ -56,5 +57,81 @@ class MenuController extends Controller
     {
         //return single menu as a resource
         return new MenuResource(true, 'Data Menu Ditemukan!', $menu);
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $menu
+     * @return void
+     */
+    public function update(Request $request, Menu $menu)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'nama_menu'   => 'required',
+            'deskripsi'   => 'required',
+            'harga'   => 'required',
+            'kategori_id'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //check if image is not empty
+        if ($request->hasFile('image')) {
+
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/menus', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/menus/'.$menu->image);
+
+            //update menu with new image
+            $menu->update([
+                'image'     => $image->hashName(),
+                'nama_menu'     => $request->nama_menu,
+                'deskripsi'   => $request->deskripsi,
+                'harga'   => $request->harga,
+                'kategori_id'   => $request->kategori_id,
+            ]);
+
+        } else {
+
+            //update menu without image
+            $menu->update([
+                'image'     => $image->hashName(),
+                'nama_menu'     => $request->nama_menu,
+                'deskripsi'   => $request->deskripsi,
+                'harga'   => $request->harga,
+                'kategori_id'   => $request->kategori_id,
+            ]);
+        }
+
+        //return response
+        return new MenuResource(true, 'Data Menu Berhasil Diubah!', $menu);
+    }
+
+    /**
+     * destroy
+     *
+     * @param  mixed $menu
+     * @return void
+     */
+    public function destroy(Menu $menu)
+    {
+        //delete image
+        Storage::delete('public/menus/'.$menu->image);
+
+        //delete post
+        $menu->delete();
+
+        //return response
+        return new MenuResource(true, 'Data Menu Berhasil Dihapus!', null);
     }
 }
